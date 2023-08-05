6c771dfbf9f0328b0e06fd76d741859c6f9173f1
Erik Sundberg <erik.sundberg@unicus.com>
2023-08-05 15:34:55 +0200

    move types

--- store/redux/favoritesSlice.ts
+++ store/redux/favoritesSlice.ts
@@ -1,17 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-type FavoriteAction = {
-  type: string;
-  payload: {
-    id: string;
-  };
-};
-
-type FavoritesState = {
-  favorites: {
-    mealIds: string[];
-  };
-};
 
 const favoritesSlice = createSlice({
   name: 'favorites',
@@ -26,8 +14,8 @@ const favoritesSlice = createSlice({
     removeFavorite: (state, action: FavoriteAction) => {
       state.mealIds = state.mealIds.filter(
         (mealId) => mealId !== action.payload.id
-      );
-    },
+        );
+      },
   },
 });
 
@@ -35,4 +23,17 @@ export const { addFavorite, removeFavorite } = favoritesSlice.actions;
 export const favoritesReducer = favoritesSlice.reducer;
 
 export const selectFavoriteMealIds = (state: FavoritesState) =>
-  state.favorites.mealIds;
+state.favorites.mealIds;
+
+type FavoriteAction = {
+  type: string;
+  payload: {
+    id: string;
+  };
+};
+
+type FavoritesState = {
+  favorites: {
+    mealIds: string[];
+  };
+};
